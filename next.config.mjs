35a290f2054c760016481293171be6e951cceb83
Jimmy-Martin34 <jimmy@thelandsquad.com>
2025-09-10 09:14:14 -0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // if you use remote images, add domains here:
+  // images: { domains: ["images.example.com"] },
+};
+
+export default nextConfig;
